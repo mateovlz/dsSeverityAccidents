b0ccd86da10413b4c6ae1fd3bafc153e5e242891
Mateo Velez <mateoande-99@hotmail.com>
2021-07-22 00:40:40 -0500

    add: Creating structure of flask project

---
+++ accidents/__init__.py
@@ -0,0 +1,24 @@
+import os
+
+from flask import Flask
+
+
+def create_app(test_config=None):
+    app = Flask(__name__, instance_relative_config=True)
+
+    if test_config is None:
+        app.config.from_pyfile('config.py',silent=True)
+    else:
+        app.config.from_mapping(test_config)
+
+    # Ensure the instance folder exists
+    try:
+        os.makedirs(app.auto_find_instance_path())
+    except OSError:
+        pass
+
+    @app.route('/hello')
+    def hello():
+        return 'Hello, World!'
+
+    return app
